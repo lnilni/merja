c348cfa0a86e85a3199ab2fce3cc968642d2ae46
lni_T <developer.lni@gmail.com>
2014-11-25 04:44:04 +0900

    JSONファイルを読み込んでhash化する処理を実装(#4)

--- spec/lib/merja/pathname_spec.rb
+++ spec/lib/merja/pathname_spec.rb
@@ -3,4 +3,11 @@ require 'spec_helper'
 require 'merja'
 
 describe Merja::Pathname do
+  describe Merja::Pathname::Json do
+    let(:file) { described_class.new("spec/dummy_merja/public/json/test.json") }
+    describe "#to_hash" do
+      subject { file.to_hash }
+      it { should == {"teststr" => "test", "testnum" => 1} }
+    end
+  end
 end
