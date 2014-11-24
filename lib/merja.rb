ca248df2cab161be98ef00fa0bb8c44e35a4aadd
lni_T <developer.lni@gmail.com>
2014-11-25 00:08:10 +0900

    Pathnameを無害化する機能を実装(#3)

--- lib/merja.rb
+++ lib/merja.rb
@@ -1,9 +1,19 @@
 require "merja/engine"
 
 module Merja
+  class ForbiddenError < StandardError ; end
+
   class << self
+  
+  private
     def target_pathname(path)
       ::Pathname.new(accessible_dir + path)
+    end
+
+    def sanitize(pathname)
+      cleanpath = pathname.cleanpath
+      raise ForbiddenError if cleanpath.to_s !~ %r(^#{accessible_dir.cleanpath.to_s})
+      cleanpath
     end
 
     def accessible_dir
--- spec/lib/merja_spec.rb
+++ spec/lib/merja_spec.rb
@@ -3,13 +3,29 @@ require 'spec_helper'
 require 'merja'
 
 describe Merja do
-  before do
-    allow(::Rails).to receive(:root).and_return(Pathname.new("./spec/dummy_merja/").expand_path)
-    #Merja.stub(:accessible_dir).and_return(Pathname.new("/dummy/public/"))
-  end
+  before { allow(::Rails).to receive(:root).and_return(Pathname.new("./spec/dummy_merja/").expand_path) }
 
   describe "#target_pathname" do
     subject { Merja.__send__(:target_pathname, "hoge") }
     it { should == Pathname.new("./spec/dummy_merja/public/hoge").expand_path }
+  end
+
+  describe "#sanitize" do
+    subject { Merja.__send__(:sanitize, Pathname.new(path).expand_path) }
+
+    context "pathがpublic配下の場合" do
+      let(:path) { "./spec/dummy_merja/public/hoge" }
+      it { should == Pathname.new(path).expand_path }
+    end
+
+    context "pathがpublic配下でない場合" do
+      let(:path) { "./spec/dummy_merja/app" }
+      it { expect { subject }.to raise_error Merja::ForbiddenError }
+    end
+
+    context "pathに../を使用してディレクトリトラバーサルしようとした場合" do
+      let(:path) { "./spec/dummy_merja/public/../../" }
+      it { expect { subject }.to raise_error Merja::ForbiddenError }
+    end
   end
 end
